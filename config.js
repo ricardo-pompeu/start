d7a0366ed4a11ebca1a6cdd2c20a2a09fc9f3c0e
ricardo-pompeu <149098706+ricardo-pompeu@users.noreply.github.com>
2024-08-12 22:40:10 -0300

    Update config.js

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ const CONFIG = {
 	weatherKey: '88687897be6686358361d885f0c9253e', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
-	language: 'en', // More languages in https://openweathermap.org/current#multi
+	language: 'pt_br', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
 	defaultLatitude: '-1.454980',
